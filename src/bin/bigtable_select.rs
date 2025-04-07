bb383c9f2174c0bbf1569543e4bf22bc0cc24bcf
Peter Arato <it.arato@gmail.com>
2025-04-07 07:00:51 -0400

    Add filters base.

---
+++ src/bin/bigtable_select.rs
@@ -0,0 +1,26 @@
+use std::path::PathBuf;
+
+use pbase::{
+    common::Error,
+    pbase::PBase,
+    query::{FieldSelector, SelectQuery},
+};
+
+fn main() -> Result<(), Error> {
+    let db = PBase::new(std::env::current_dir().unwrap_or_else(|_| PathBuf::new()));
+
+    let query = SelectQuery {
+        // result: vec![FieldSelector {
+        //     name: "field1".into(),
+        //     source: "bigtable".into(),
+        // }],
+        from: "bigtable".into(),
+        filters: vec![],
+        // limit: None,
+    };
+
+    let result = db.run_select_query(query);
+    dbg!(result);
+
+    Ok(())
+}
