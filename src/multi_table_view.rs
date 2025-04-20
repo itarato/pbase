1ffc97470d6be326566acaea896404acb4432c8e
Peter Arato <it.arato@gmail.com>
2025-04-20 14:31:10 -0400

    test

--- src/multi_table_view.rs
+++ src/multi_table_view.rs
@@ -15,9 +15,8 @@ impl MultiTableView {
         table_bytes: &[u8],
         table_schema: &TableSchema,
         selection: &Selection,
-        table_name: String,
     ) -> MultiTableView {
-        let tables = HashMap::from([(table_name, 0)]);
+        let tables = HashMap::from([(table_schema.name.clone(), 0)]);
 
         let view = match selection {
             Selection::All => {
@@ -37,4 +36,65 @@ impl MultiTableView {
 }
 
 #[cfg(test)]
-mod test {}
+mod test {
+    use std::collections::HashMap;
+
+    use indexmap::IndexMap;
+
+    use crate::schema::{FieldSchema, TableSchema};
+
+    use super::MultiTableView;
+
+    #[test]
+    fn test_row_pos_with_an_all_selection() {
+        let table_schema = TableSchema {
+            name: "t1".to_string(),
+            fields: IndexMap::from([
+                ("f1".to_string(), FieldSchema::I32),
+                ("f2".to_string(), FieldSchema::I32),
+            ]),
+            indices: HashMap::new(),
+        };
+
+        #[rustfmt::skip]
+        let table_bytes: [u8; 16] = [
+            1, 0, 0, 0,   2, 0, 0, 0, // Row 1
+            3, 0, 0, 0,   4, 0, 0, 0, // Row 2
+        ];
+
+        let view = MultiTableView::new_from_table_bytes_and_selection(
+            &table_bytes,
+            &table_schema,
+            &crate::common::Selection::All,
+        );
+
+        assert_eq!(0, view.row_pos(0, "t1"));
+        assert_eq!(8, view.row_pos(1, "t1"));
+    }
+
+    #[test]
+    fn test_row_pos_with_a_list_selection() {
+        let table_schema = TableSchema {
+            name: "t1".to_string(),
+            fields: IndexMap::from([
+                ("f1".to_string(), FieldSchema::I32),
+                ("f2".to_string(), FieldSchema::I32),
+            ]),
+            indices: HashMap::new(),
+        };
+
+        #[rustfmt::skip]
+        let table_bytes: [u8; 16] = [
+            1, 0, 0, 0,   2, 0, 0, 0, // Row 1
+            3, 0, 0, 0,   4, 0, 0, 0, // Row 2
+        ];
+
+        let view = MultiTableView::new_from_table_bytes_and_selection(
+            &table_bytes,
+            &table_schema,
+            &crate::common::Selection::List(vec![8]),
+        );
+
+        assert_eq!(8, view.row_pos(0, "t1"));
+    }
+}
